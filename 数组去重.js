6694b38f8ff62ad0f3d30d9518a290f4744afe05
e <578755236@qq.com>
2020-06-07 22:44:32 +0800

    去重

--- 数组去重.js
+++ 数组去重.js
@@ -57,3 +57,25 @@ function unique(arr) {
     }
     return res
 }
+
+// unique api
+
+var array1 = [1, 2, '1', 2, 1]
+var array2 = [1, 1, '1', 2, 2]
+
+function unique(arr, isSorted) {
+    var res = []
+    var seen = []
+
+    for(var i = 0, len = array.length;i < len;i++) {
+        var value = array[i]
+        if(isSorted) {
+            if(!i || seen !== value) {
+                res.push(value)
+            }
+        } else if(res.indexOf(value) === -1) {
+            res.push(value)
+        }
+    }
+    return res
+}
