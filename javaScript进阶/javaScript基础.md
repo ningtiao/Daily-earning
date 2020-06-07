e807fc88d1f953422b046570121d60970a98a9a9
e <578755236@qq.com>
2020-06-07 22:42:20 +0800

    数组去重

---
+++ javaScript进阶/javaScript基础.md
@@ -0,0 +1,4 @@
+### JavaScript基础
+
+#### 变量和类型
+- 1 JavaScript规定了几种语言类型
--- 数组去重.js
+++ 数组去重.js
@@ -23,12 +23,13 @@ function unique(arr) {
     return [...new Set(arr)]
 }
 
+var unique = (a) => [...new Set(a)]
 // Object
 
 function unique(arr) {
     let obj = {}
     return arr.filter((item,index, arr) => {
-        return obj.getOwnProperty(item) ? false : (obj[item] = true)
+        return obj.getOwnProperty(typeof item + item) ? false : (obj[typeof item + item] = true)
     })
 }
 
@@ -38,4 +39,21 @@ function unique(arr) {
     return arr.filter((item, index, arr) => {
         return arr.indexOf(item) === index
     })
+}
+
+// 多维数组 去重依然是多维数组
+
+var array = [1, 2, 1, [1, 1, 2], [3, 3, 4]];
+
+function unique(arr) {
+    var res = []
+    for(var i = 0, len = array.length; i< len;i++) {
+        let current = arr[i]
+        if (Array.isArray(array[i])) {
+            res.push(unique(array[i]))
+        } else if (res.indexOf(current) === -1) {
+            res.push(current)
+        }
+    }
+    return res
 }
