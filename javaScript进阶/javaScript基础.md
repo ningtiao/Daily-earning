9c81e5b3aaa5e2603544f55ad3ea070783be85c6
e <578755236@qq.com>
2020-07-02 23:30:54 +0800

    继承

--- javaScript进阶/javaScript基础.md
+++
@@ -1,4 +0,0 @@
-### JavaScript基础
-
-#### 变量和类型
-- 1 JavaScript规定了几种语言类型
--- javaScript进阶/原型.js
+++
@@ -1,9 +0,0 @@
-function Person() {}
-
-Person.prototype.name = 'Kevin'
-
-var person1 = new Person()
-
-var person2 = new Person()
-console.log(person1.name)
-console.log(person2.name)
