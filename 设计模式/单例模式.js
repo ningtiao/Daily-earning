ebea2be755653f45d15590fa9a65e92668c5fdbe
yjx <578755236@qq.com>
2020-08-17 22:50:20 +0800

    设计模式

--- 设计模式/单例模式.js
+++ 设计模式/单例模式.js
@@ -26,3 +26,34 @@ storage1.getItem('name')
 storage2.getItem('name')
 
 storage1 === storage2 // true
+
+
+// 闭包版本
+
+function StorageBase() {}
+
+StorageBase.prototype.getItem = function (key) {
+  return localStorage.getItem(key)
+}
+
+StorageBase.prototype.setItem = function(key, val) {
+  return localStorage.setItem(key, value)
+}
+
+const Storage = (function() {
+  let instance = null
+  return function() {
+    if (!instance) {
+      instance = new StorageBase()
+    }
+    return instance
+  }
+})()
+
+const storage1 = new Storage()
+storage1.setItem('name', 'lilei')
+storage1.getItem('name')
+
+storage2.getItem('name')
+
+storage1 === storage2 // true
