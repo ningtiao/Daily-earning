a154a8fc95b788008542294b18e1a4d7a3667e7f
yjx <578755236@qq.com>
2020-08-28 00:20:03 +0800

    sleep函数

---
+++ javaScript进阶/es7.js
@@ -0,0 +1,34 @@
+const tasks = [];
+
+const output = (i) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log(new Date, i)
+    resolve()
+  }, 1000 * i)
+})
+
+for (var i = 0; i < 5; i++) {
+  tasks.push(output(i))
+}
+
+Promise.all(tasks).then(() => {
+  setTimeout(() => {
+    console.log(new Date, i)
+  }, 1000)
+})
+
+
+const sleep = (timeOuntMS) => new Promise((resolve) => {
+  setTimeout(resolve, timeOuntMS)
+})
+
+( async () => {
+  for (var i = 0; i< 5; i++) {
+    if (i > 0) {
+      await sleep(1000)
+    }
+    console.log(new Date, i)
+  }
+  await sleep(1000)
+  console.log(new Date, i)
+})
